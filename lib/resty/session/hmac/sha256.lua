f9c94fd269ab00c736128e3c5d19fff8c0cd1b80
Jesse Cooper <jesse.cooper@sciencelogic.com>
2021-04-22 10:35:00 -0500

    add sha256 hmac digest

---
+++ lib/resty/session/hmac/sha256.lua
@@ -0,0 +1,9 @@
+
+local function hmac_sha256(secret, input)
+    local hmac = require("resty.hmac")
+    local digest = hmac:new(secret, hmac.ALGOS.SHA256)
+
+    return digest:final(input, false)
+end
+
+return hmac_sha256
